9b9fc34ac083f24893d1704f23e988965b76fff4
fys <1113014250@qq.com>
2020-12-31 20:03:39 +0800

    fix bug in CmdConfigService.java

--- fNet-common/src/main/java/com/fnet/common/config/cmd/CmdConfigService.java
+++ fNet-common/src/main/java/com/fnet/common/config/cmd/CmdConfigService.java
@@ -54,6 +54,7 @@ public class CmdConfigService implements ConfigService {
             Config.OUTER_SERVER_PORT = Integer.parseInt(commandLine.getOptionValue("port", String.valueOf(Config.DEFAULT_OUTER_SERVER_PORT)));
             Config.PASSWORD = commandLine.getOptionValue("password", Config.DEFAULT_PASSWORD);
             Config.OUTER_REMOTE_PORT = Integer.parseInt(commandLine.getOptionValue("remotePort", String.valueOf(Config.DEFAULT_OUTER_REMOTE_PORT)));
+            Config.TRANSFER_CHANNEL_NUMBERS = Integer.parseInt(commandLine.getOptionValue("nt", String.valueOf(Config.DEFAULT_TRANSFER_CHANNEL_NUMBERS)));
         }
     }
 }
