fa5aa1a8982b9bd79b8385bc2b975a918cdee311
fys <1113014250@qq.com>
2021-01-01 17:50:24 +0800

    add ThreadPoolUtil.java

---
+++ fNet-common/src/main/java/com/fnet/common/service/ThreadPoolUtil.java
@@ -0,0 +1,37 @@
+package com.fnet.common.service;
+
+import io.netty.util.NettyRuntime;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class ThreadPoolUtil {
+
+    private static class NewThreadPolicy implements RejectedExecutionHandler {
+
+        public NewThreadPolicy() { }
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
+            log.info("common thread pool resource insufficient, so create a new thread to excute!");
+            new Thread(r).start();
+        }
+    }
+
+    private static int availableProcessors = NettyRuntime.availableProcessors();
+    private static final ExecutorService commonExecutor = new ThreadPoolExecutor(availableProcessors, availableProcessors * 2,
+                                                                                  5L, TimeUnit.SECONDS,
+                                                                                 new LinkedBlockingQueue<Runnable>(1000),
+                                                                                 new DefaultThreadFactory("commonThreadPool"),
+                                                                                 new NewThreadPolicy());
+
+    public ExecutorService getCommonExecutor() {
+        return commonExecutor;
+    }
+}
